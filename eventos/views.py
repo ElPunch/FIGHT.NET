88b74366d08169aacb68670268925e546d00fc8a
ElPunch <alfonsorobledo3548@gmail.com>
2025-08-10 20:00:20 -0600

    Refactor event creation logic in paginaView to use JSON data and return a JSON response

--- eventos/views.py
+++ eventos/views.py
@@ -4,6 +4,7 @@ import json
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from .models import Eventos, Usuarios
+from datetime import datetime
 
 from django.shortcuts import render
 
@@ -37,17 +38,17 @@ class paginaView(View):
     
     def post(self, request):
         try:
-            evento = Eventos.objects.create(
-                nombre=request.POST.get('nombre'),
-                descripcion=request.POST.get('descripcion'),
-                localizacion=request.POST.get('localizacion'),
-                organizador=request.POST.get('organizador'),
-                disciplina=request.POST.get('disciplina'),
-                data=datetime.strptime(request.POST.get('data'), '%Y-%m-%d').date(),
-                usuario_id=1  # Cambiar por el ID del usuario logueado
+            data = json.loads(request.body)
+            eventos = Eventos.objects.create(
+                nombre=data["nombre"],
+                descripcion=data["descripcion"],
+                localizacion=data["localizacion"],
+                organizador=data["organizador"],
+                disciplina=data["disciplina"],
+                usuario_id=data["usuario"], 
+                data=data["data"]
             )
-            messages.success(request, f'Evento "{evento.nombre}" creado exitosamente!')
-            return redirect('pagina_eventos')  # Redireccionar a la misma página
-        except Exception as e:
-            messages.error(request, f'Error al crear el evento: {str(e)}')
-            return redirect('pagina_eventos')
+            return JsonResponse({"mensaje": "Evento creado correctamente", "id": eventos.id})
+        except:
+            return HttpResponseBadRequest("Error al crear el evento")
+    
